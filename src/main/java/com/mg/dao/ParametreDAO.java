694382513515d3e08c66cff7dde1217acafadfa0
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-05 23:28:01 +0300

    Update ParametreDAO.java

--- src/main/java/com/mg/dao/ParametreDAO.java
+++ src/main/java/com/mg/dao/ParametreDAO.java
@@ -5,19 +5,17 @@ import com.mg.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
-public class ParametreDAO implements GenericDAO<Parametre> {
+public class ParametreDAO extends BaseDao<Parametre> {
+
+    public ParametreDAO() {
+        super(Parametre.class);
+    }
 
     public Parametre findFirst() {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Parametre> query = session.createQuery("FROM Parametre", Parametre.class);
             query.setMaxResults(1);
             return query.uniqueResult();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
