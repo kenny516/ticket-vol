e2b80c55960261829f6b0a4e395d9ddaddb62ebf
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-05 23:27:59 +0300

    Update AvionDAO.java

--- src/main/java/com/mg/dao/AvionDAO.java
+++ src/main/java/com/mg/dao/AvionDAO.java
@@ -1,7 +1,12 @@
 package com.mg.dao;
 
 import com.mg.model.Avion;
-public class AvionDAO implements GenericDAO<Avion> {
+
+public class AvionDAO extends BaseDao<Avion> {
+
+    public AvionDAO() {
+        super(Avion.class);
+    }
 
     // On peut ajouter des méthodes spécifiques pour la recherche d'avions si
     // nécessaire
