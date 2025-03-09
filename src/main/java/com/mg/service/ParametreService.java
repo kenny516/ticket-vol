0bfc81acdb29454d3c7dbcdd43ac429161cbe45b
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-09 15:06:32 +0300

    Update ParametreService.java

--- src/main/java/com/mg/service/ParametreService.java
+++ src/main/java/com/mg/service/ParametreService.java
@@ -8,33 +8,38 @@ public class ParametreService extends AbstractService<Parametre> {
 
     public ParametreService() {
         super(new ParametreDAO());
-        this.parametreDAO = (ParametreDAO) dao;
+        this.parametreDAO = new ParametreDAO();
     }
 
-    public Parametre getParametres() {
-        Parametre parametre = parametreDAO.findFirst();
-        if (parametre == null) {
-            parametre = new Parametre();
-            parametre.setHeuresMinimumReservation(24); // Default value
-            parametre.setHeuresMinimumAnnulation(48); // Default value
-            parametreDAO.save(parametre);
-        }
-        return parametre;
+    public Parametre findById(Class<Parametre> type, Object id) {
+        return parametreDAO.findById(type, id);
     }
 
-    public void updateParametres(Integer heuresMinimumReservation, Integer heuresMinimumAnnulation) {
-        Parametre parametre = parametreDAO.findFirst();
+    public void saveParametre(String cle, Double valeur) {
+        Parametre parametre = findById(Parametre.class, cle);
         if (parametre == null) {
             parametre = new Parametre();
+            parametre.setCle(cle);
         }
-
-        parametre.setHeuresMinimumReservation(heuresMinimumReservation);
-        parametre.setHeuresMinimumAnnulation(heuresMinimumAnnulation);
-
-        if (parametre.getId() == null) {
+        parametre.setValeur(valeur);
+        if (parametre.getCle() == null) {
             parametreDAO.save(parametre);
         } else {
             parametreDAO.update(parametre);
         }
+    }
+
+    public Double getValeurParametre(String cle, Double defaultValue) {
+        Parametre parametre = findById(Parametre.class, cle);
+        return parametre != null ? parametre.getValeur() : defaultValue;
+    }
+
+    public void updateDelais(Double delaiReservation, Double delaiAnnulation) {
+        saveParametre("delai_reservation", delaiReservation);
+        saveParametre("delai_annulation", delaiAnnulation);
+    }
+
+    public void updateReductionEnfant(Double reduction) {
+        saveParametre("reduc_enfant", reduction);
     }
 }
