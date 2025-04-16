cbaf5e9007aa85146697b17d498b7b6777e3fe9c
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-04-16 11:48:24 +0300

    Update PlaceVolDAO.java

--- src/main/java/com/mg/dao/PlaceVolDAO.java
+++ src/main/java/com/mg/dao/PlaceVolDAO.java
@@ -1,10 +1,36 @@
 package com.mg.dao;
 
 import com.mg.model.PlaceVol;
+import com.mg.utils.HibernateUtil;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class PlaceVolDAO extends BaseDao<PlaceVol> {
 
     public PlaceVolDAO() {
         super(PlaceVol.class);
     }
+
+
+
+    // add function to find placeVol by idVol and place
+    public PlaceVol findByIdVolAnIdPlace(Integer idVol, Integer idPlace) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String hql = "FROM PlaceVol pv WHERE pv.vol.id = :idVol AND pv.place.id = :idPlace";
+            return session.createQuery(hql, PlaceVol.class)
+                    .setParameter("idVol", idVol)
+                    .setParameter("idPlace", idPlace)
+                    .uniqueResult();
+        }
+    }
+
+    public List<PlaceVol> findByIdVol(Integer idVol) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String hql = "FROM PlaceVol pv WHERE pv.vol.id = :idVol";
+            return session.createQuery(hql, PlaceVol.class)
+                    .setParameter("idVol", idVol).list();
+        }
+    }
+
 }
