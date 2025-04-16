546a1d7032bba756dd0defe7ab086932df18bf0f
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-04-16 11:48:35 +0300

    Update PlaceService.java

--- src/main/java/com/mg/service/PlaceService.java
+++ src/main/java/com/mg/service/PlaceService.java
@@ -5,57 +5,38 @@ import com.mg.dao.AvionDAO;
 import com.mg.dao.TypeSiegeDAO;
 import com.mg.model.Place;
 import com.mg.model.Avion;
+import com.mg.model.PlaceVol;
 import com.mg.model.TypeSiege;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PlaceService extends AbstractService<Place> {
     private final PlaceDAO placeDAO;
     private final AvionDAO avionDAO;
     private final TypeSiegeDAO typeSiegeDAO;
+    private final PlaceVolService placeVolService;
 
     public PlaceService() {
         super(new PlaceDAO());
         this.placeDAO = (PlaceDAO) dao;
         this.avionDAO = new AvionDAO();
         this.typeSiegeDAO = new TypeSiegeDAO();
+        this.placeVolService = new PlaceVolService();
     }
 
     public List<Place> findByAvion(Integer avionId) {
         return placeDAO.findByAvion(avionId);
     }
 
-    public List<Place> findByAvionAndTypeSiege(Integer avionId, Integer typeSiegeId) {
-        return placeDAO.findByAvionAndTypeSiege(avionId, typeSiegeId);
-    }
+    public List<Place> findByAvionForVol(Integer avionId,Integer volId) {
+        List<Place> places = placeDAO.findByAvion(avionId);
+        List<PlaceVol> placeVols = placeVolService.findByVol(volId);
 
-    public Place createPlace(Integer avionId, Integer typeSiegeId, Integer nombre) {
-        Place place = new Place();
-        place.setAvion(avionDAO.findById(Avion.class, avionId));
-        place.setTypeSiege(typeSiegeDAO.findById(TypeSiege.class, typeSiegeId));
-        place.setNombre(nombre);
-        
-        placeDAO.save(place);
-        return place;
-    }
 
-    public void updatePlace(Integer id, Integer typeSiegeId, Integer nombre) {
-        Place place = placeDAO.findById(Place.class, id);
-        if (place != null) {
-            if (typeSiegeId != null) {
-                place.setTypeSiege(typeSiegeDAO.findById(TypeSiege.class, typeSiegeId));
-            }
-            if (nombre != null) {
-                place.setNombre(nombre);
-            }
-            placeDAO.update(place);
-        }
-    }
-
-    public void deletePlace(Integer id) {
-        Place place = placeDAO.findById(Place.class, id);
-        if (place != null) {
-            placeDAO.delete(place);
-        }
+        return places.stream().filter(place -> placeVols.stream().
+                noneMatch(placeVol -> Objects.equals(place.getTypeSiege().getId(),placeVol.getPlace().getTypeSiege().getId()))
+        ).collect(Collectors.toList());
     }
 
     public Place findById(Integer id) {
